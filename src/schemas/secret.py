5d7b15034fd0dbc15cde9dd9e0e45de2b6d16bb1
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-12-14 23:46:20 +0300

    added all schemas

---
+++ src/schemas/secret.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from constants.secret import (
+    MAX_SECRET_LENGTH,
+    MAX_SECRET_NAME_LENGTH,
+    MIN_SECRET_LENGTH,
+    MIN_SECRET_NAME_LENGTH,
+)
+from schemas.paginate import PaginatedResponseBase
+
+
+class SecretBase(BaseModel):
+    class Config:
+        from_attributes = True
+
+
+class SecretCreate(SecretBase):
+    name: str = Field(
+        min_length=MIN_SECRET_NAME_LENGTH, max_length=MAX_SECRET_NAME_LENGTH
+    )
+    secret: str = Field(
+        min_length=MIN_SECRET_LENGTH, max_length=MAX_SECRET_LENGTH
+    )
+
+
+class SecretCreateDB(SecretBase):
+    secret: bytes
+    owner_id: int
+
+    class Config:
+        from_attributes = True
+
+
+class SecretResponse(SecretBase):
+    id: int
+    name: str
+
+
+class SecretFullResponse(SecretCreate):
+    id: int
+
+
+class SecretPaginatedResponse(PaginatedResponseBase):
+    objects: List[SecretResponse] = []
