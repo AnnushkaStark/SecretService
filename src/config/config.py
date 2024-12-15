fc732d1da6e27e1a2a3f8c2bebfd6992040407ce
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-12-15 22:35:21 +0300

    added db settings

---
+++ src/config/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from .base import BaseSetting
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class DBSettings(BaseSetting):
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
--- src/databases/database.py
+++ src/databases/database.py
@@ -1,22 +1,25 @@
 from typing import AsyncGenerator
 
+from sqlalchemy import Integer
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql.sqltypes import ARRAY, String
+
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.sql.sqltypes import ARRAY, Integer, String
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./secret.db"
+SQLALCHEMY_DATABASE_URL = ("postgresql+asyncpg://secret:secret@localhost/secret")
 
 async_engine = create_async_engine(
-    SQLALCHEMY_DATABASE_URL, echo=True, pool_pre_ping=True
+    url=SQLALCHEMY_DATABASE_URL, pool_size=100, max_overflow=20, echo=True
 )
 
-
 async_session = async_sessionmaker(
-    async_engine, class_=AsyncSession, expire_on_commit=False
+    async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
 )
 
 
@@ -26,7 +29,6 @@ class Base(DeclarativeBase):
         list[str]: ARRAY(String),
         list[int]: ARRAY(Integer),
     }
-
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
